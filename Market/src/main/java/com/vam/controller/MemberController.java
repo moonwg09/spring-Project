cc5d0e6d8518dc0245e800b8c17ff0db328aa842
moonwg09 <moonwg09@naver.com>
2023-11-14 20:45:51 +0900

    no message

--- Market/src/main/java/com/vam/controller/MemberController.java
+++ Market/src/main/java/com/vam/controller/MemberController.java
@@ -19,27 +19,12 @@ public class MemberController {
 
 	@Autowired
 	MemberService memberservice;
-	
-	@Autowired
-	private BCryptPasswordEncoder pwEncoder;
+
 
 	// 회원가입 버튼 클릭 시
 	@RequestMapping(value = "/join", method = RequestMethod.GET)
 	public void memberJoin() throws Exception {
 		logger.info("memberJoin 진입");
-	}
-
-	// 회원가입
-	@RequestMapping(value = "/join", method = RequestMethod.POST)
-	public String joinPost(MemberVO mvo) throws Exception {
-		String rawPw = ""; // 인코딩 전
-		String encodePw = ""; // 인코딩 후
-
-		rawPw = mvo.getPassword(); // 비밀번호 데이터 얻음
-		encodePw = pwEncoder.encode(rawPw); // 비밀번호 인코딩
-		mvo.setPassword(encodePw); // 인코딩된 비밀번호 member객체에 다시 저장
-		 memberservice.memberJoin(mvo);
-		return "redirect:/login";
 	}
 
 	// 로그인 버튼 클릭 시
